6734819ec5eac8b55b9dc8a78d07a219d4f92751
David Metcalfe <dametcalfe@gmail.com>
2017-02-01 19:50:38 -0800

    File commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Windows 10 Lock Screen Image Lookup
+
+A simple script that will fetch the currently displayed Lock Screen wallpaper being used by Windows 10 and do a Google image search to help identify it. Resulting search will pop up in your default browser.
+
+This came about as a response to the poor UI/UX design by Microsoft where the image information is shown only prior to the login screen showing. For those like myself that wake their computers through keyboard input, this made it extremely tedious to try to learn more about people's wonderful photography and places around the world.
+
+## Installation
+
+Requires the wonderful Requests module to run. 
+
+Open a terminal in the script's directory and type `pip install -r requirements.txt` to install it.
+
+## Usage
+
+1. Run the script with `python application.py` in the folder containing the script.
+2. Enjoy the eye-candy.
+
+## Contributing
+
+Contributions always welcome.
+
+## License
+
+GNU GPL v3
---
+++ application.py
@@ -0,0 +1,32 @@
+import winreg
+import os.path
+import webbrowser
+
+import requests
+
+'''
+A simple script that will fetch the currently displayed Lock Screen 
+wallpaper being used by Windows 10 and do a Google image search 
+to help identify it. Resulting search will pop up in your default browser.
+
+David Metcalfe, Feb 1 2017
+'''
+
+# Connect to registry, set appropriate key.
+reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
+reg = winreg.OpenKey(reg,
+    r"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Lock Screen\\Creative")
+
+# Fetch Registry value (file path) for LandscapeAssetPath.
+regKey = winreg.QueryValueEx(reg, 'LandscapeAssetPath')[0]
+
+# Normalize filepath.
+imgDir = os.path.normpath(regKey)
+
+# Hand-off file to Google Images for a reverse image search.
+filePath = imgDir
+searchUrl = 'http://www.google.hr/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
+webbrowser.open(fetchUrl)
